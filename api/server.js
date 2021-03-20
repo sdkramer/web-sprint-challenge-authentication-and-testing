1d7ec5ae5b354e06837d20c88231d65e8f16d20d
Sean Kramer <seandominickramer@gmail.com>
2021-03-20 14:17:31 -0400

    Add dependencies

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 
 const restrict = require('./middleware/restricted.js');
 
